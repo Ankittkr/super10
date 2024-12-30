d29e947b510fb98592deb7fa54dcaff37682af5c
Ankittkr <ankittkr21@gmail.com>
2024-12-30 19:30:21 +0530

    super10-hospital_project-"database table of doctors" application files

--- hospital_project/doctors/urls.py
+++ hospital_project/doctors/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from doctors import views
 urlpatterns = [
     path('doctors/' , views.doctors_page , name="doctors_page"),
+    path('doctorsdata/' , views.doctors_data , name="doctors_data"),
+ 
 ]
--- hospital_project/doctors/views.py
+++ hospital_project/doctors/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from doctors.models import doctorsinfo
 # Create your views here.
 def doctors_page(request):
     return render(request,"doctors.html")
+
+def doctors_data(request):
+    ddata = doctorsinfo.objects.all().values()
+    context = {
+        "ddata" :  ddata
+    }
+    return render(request, "doctors_data.html" , context)
